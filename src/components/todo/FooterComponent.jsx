093e77fcf00d458b71746ce5933b8a65fbabe90d
nerdnightboy <nerdnightboy@gmail.com>
2024-07-17 17:01:20 +0530

    little update in footer

--- src/components/todo/FooterComponent.jsx
+++ src/components/todo/FooterComponent.jsx
@@ -2,10 +2,10 @@ export default function FooterComponent() {
 
     // console.log(`Footer Component - ${authContext.number}`);
     return (
-        <footer className="footer bg-dark text-white mt-5 py-4">
-            <div className="container text-center">
-                <p className="fs-5">Your Footer</p>
-                <small>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</small>
+        <footer className="footer bg-dark text-white">
+            <div className="container text-center d-flex flex-column justify-content-center" style={{ height: '100%' }}>
+                <p className="fs-5 mb-0">Your Footer</p>
+                <small className="mt-1">&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</small>
             </div>
         </footer>
     );
