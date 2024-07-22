3f52cac689cffcbd6c205e1cb953be58614c981b
nerdnightboy <nerdnightboy@gmail.com>
2024-07-22 13:45:30 +0530

    ...

--- src/components/todo/FooterComponent.jsx
+++ src/components/todo/FooterComponent.jsx
@@ -4,7 +4,7 @@ export default function FooterComponent() {
     return (
         <footer className="footer bg-dark text-white">
             <div className="container text-center d-flex flex-column justify-content-center" style={{ height: '100%' }}>
-                <p className="fs-5 mb-0">Your Footer</p>
+                <p className="fs-5 mb-0">Footer</p>
                 <small className="mt-1">&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</small>
             </div>
         </footer>
